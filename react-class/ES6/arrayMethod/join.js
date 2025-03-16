4c8617d9c9d68749aea02b8f1875d7d4cc513dfb
lakshmi v <lakshmivip96@gmail.com>
2025-03-16 22:12:58 +0530

    arrayMethods

--- react-class/ES6/arrayMethod/join.js
+++ react-class/ES6/arrayMethod/join.js
@@ -1,3 +1,8 @@
 const words = ["Hello", "World"];
 console.log(words.join(" ")); 
 // Output: "Hello World"
+
+const obj = { name: "Alice", age: 25 };
+for (let key in obj) {
+  console.log(`${key}: ${obj[key]}`);
+}
--- react-class/ES6/arrayMethod/reduce.js
+++ react-class/ES6/arrayMethod/reduce.js
@@ -1,3 +1,5 @@
+//array.reduce(callback(accumulator, initialvalue, currentIndex,array),[])
+
 const data = [[1,2],[3,4],[5,6,7]]
 
 // const sum = data.reduce((acc, init) => acc + init)
@@ -9,6 +11,44 @@ const data = [[1,2],[3,4],[5,6,7]]
 // const newArry = data.flat()
 // console.log(newArry)
 
+//remove duplicates
 const arrayOne = [1,2,2,2,4,5,6,3,5,3,4,1,4]
-const dup = [...new Set(arrayOne)]
+// const dup = [...new Set(arrayOne)]
+// console.log(dup)
+const dup = arrayOne.reduce((acc, num) => {
+ if(!acc.includes(num))  acc.push(num) 
+     return acc
+},[]) //initial value should be provided here
 console.log(dup)
+
+//Find maximum number
+const big = [10,20,40,50,90,100,60];
+
+const findmax = big.reduce((acc, m) => {
+  return   acc < m ? m : acc
+})
+console.log('max',findmax)
+
+//min
+const findMin = big.reduce((acc,m, i) => {
+    return acc > m ? m : acc
+})
+console.log('min',findMin)
+
+const findIndex = big.indexOf(Math.min(...big))
+console.log(findIndex)
+
+//counting occarence of elements
+const fruits = ['apple', 'orange', 'apple','orange', 'banna','graps']
+const count = fruits.reduce((acc,fruit) => {
+    acc[fruit] = (acc[fruit] || 0) + 1
+    return acc
+},{})
+console.log(count)
+
+//flaten array
+const flat = [1,2,[3,4],[5,6]]
+const doFlat = flat.reduce((acc, flt) => {
+   return  acc.concat(flt)
+},[])
+console.log(doFlat)
